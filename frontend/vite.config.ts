10a9db8025c0dc02bdbc4c569b6f9555745ed911
Safius Sifat <safiussifat@gmail.com>
2026-03-04 21:39:59 +0600

    chore: fix config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,18 @@
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+    plugins: [tailwindcss(), sveltekit()],
+
+
+
+    preview: {
+        port: 4173,
+        allowedHosts: ['selise.notice.fit'] // <--- Add this line
+    },
+    // If you are running in dev mode (npm run dev), use this instead:
+    server: {
+        allowedHosts: ['selise.notice.fit']
+    }
+});
